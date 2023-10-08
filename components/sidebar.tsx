159d1709af9f6d85c943c8110785b7417ff06f79
Malcolm453 <malcolm@vetro.co.za>
2023-10-08 20:55:21 +0200

    integrate chat gpt for generating conversations

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -71,7 +71,7 @@ const Sidebar = () => {
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center pl-2 mb-1">
-          <div className="relative w-4/5 h-28 mr-4">
+          <div className="relative w-4/5 h-32 mr-4">
             <Image fill alt="logo" src={URL.logo_url} />
           </div>
           {/* <h1 className={cn("text-2xl font-bold", montserrat.className)}>
